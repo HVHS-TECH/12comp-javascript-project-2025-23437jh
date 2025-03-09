94e89498fe740e972f2da010e937bcaca48dffc6
23437jh <23437jh@hvhs.school.nz>
2025-03-09 23:30:23 +0000

    Good start on game project made the player and the platform

---
+++ Game.js
@@ -0,0 +1,80 @@
+/*******************************************************/
+// Create a game
+/// Written by Joseph
+/*******************************************************/
+	
+/*******************************************************/
+// setup()
+/*******************************************************/
+//for Game height 
+const GameHeight = 1000;
+// for the Canvas width
+const GameWidth = 1000;
+// for the timer 
+var GameTime = 15;
+// for the score
+var Score = 0;
+
+function setup() {
+	console.log("setup: ");
+	cnv = new Canvas (GameWidth,GameHeight);
+    //code for Player sprite 
+	Player = new Sprite( 250,250, 25,25, 'd' );
+	Player.color = 'cyan';
+    // code for Platforms
+    Platform1 = new Sprite(100,250, 300,8, 'k');
+    Platform2 = new Sprite(200,800, 300,8, 'k');
+    Platform3 = new Sprite(500,900, 300,8, 'k');
+    Platform4 = new Sprite(500,700, 300,8, 'k');
+    Platform5 = new Sprite(400,300, 300,8, 'k');
+
+    Platform1.color = 'green';
+    Platform2.color = 'white';
+    Platform3.color = 'blue';
+    Platform4.color = 'black';
+    Platform5.color = 'red';
+    //code for wall sprites
+    wallLH  = new Sprite(0, 1000, 8, 2000, 'k');
+	wallLH.color = 'black';
+    wallLH.bounciness = 1;
+	wallRH  = new Sprite(1000, 0, 8, 2000, 'k');
+	wallRH.color = 'black';
+    wallRH.bounciness = 1;
+	wallTop = new Sprite(0, 1, 2000, 8, 'k');
+	wallTop.color = 'black';
+    wallTop.bounciness = 1;
+	wallBot = new Sprite(0, 999, 2000, 8, 'k');
+	wallBot.color = 'black';
+    wallBot.bounciness = 1;
+    //adding Gravity 
+    world.gravity.y = 10;
+}
+	
+/*******************************************************/
+// draw()
+/*******************************************************/
+function draw() {
+	background('gray');
+    //KeyBoard controls 
+    if (kb.pressing('a')) {
+        // Set sprite's velocity to the left
+        Player.vel.x = -5;
+    }
+    else if (kb.pressing ('d')) {    
+        // Set sprite's velocity to the right
+        Player.vel.x = 5;
+    }
+    else if (kb.presses ('w')) {
+        // Set sprite's velocity upwards
+        Player.vel.y = -5
+    }
+    else if (kb.pressing ('s')){
+        // Set sprite's velocity downwards
+        Player.vel.y = 5
+    }
+    
+}
+
+/*******************************************************/
+//  END OF APP
+/*******************************************************/
