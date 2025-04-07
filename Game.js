8387f15a339d578e9ba75e7b5dc5cb16a2a31b3e
23437jh <23437jh@hvhs.school.nz>
2025-04-07 00:28:34 +0000

    Game is pretty much finished

--- Game.js
+++ Game.js
@@ -2,7 +2,7 @@
 // Create a game
 /// Written by Joseph
 /*******************************************************/
-	
+
 /*******************************************************/
 // setup()
 /*******************************************************/
@@ -29,6 +29,7 @@ var Background;
 var Restart;
 //starttime
 var startTime
+
 function preload() {
     Coinimage = loadImage('Images/Goldcoin.png')
     PlatformGrass = loadImage('Images/GrassPlatform.png')
@@ -36,28 +37,28 @@ function preload() {
 }
 
 function setup() {
-	console.log("setup: ");
-	cnv = new Canvas (GameWidth,GameHeight);
+    console.log("setup: ");
+    cnv = new Canvas(GameWidth, GameHeight);
     // surfaces you can jump on
     Jumpsurfaces = new Group();
     // all the sprites 
     Sprites = new Group();
 
     //code for Player sprite 
-	Player = new Sprite( 250,250,25,25, 'd' );
-	Player.color = 'cyan';
+    Player = new Sprite(250, 250, 25, 25, 'd');
+    Player.color = 'cyan';
     //code for wall sprites
-    wallL  = new Sprite(0, GameHeight, 8, GameHeight*2, 'k');
-	wallL.color = 'black';
+    wallL = new Sprite(0, GameHeight, 8, GameHeight * 2, 'k');
+    wallL.color = 'black';
     wallL.bounciness = 1;
-	wallRH  = new Sprite(GameWidth, 0, 8, GameHeight*2, 'k');
-	wallRH.color = 'black';
+    wallRH = new Sprite(GameWidth, 0, 8, GameHeight * 2, 'k');
+    wallRH.color = 'black';
     wallRH.bounciness = 1;
-	wallTop = new Sprite(0, 0, GameWidth*2, 8, 'k');
-	wallTop.color = 'black';
+    wallTop = new Sprite(0, 0, GameWidth * 2, 8, 'k');
+    wallTop.color = 'black';
     wallTop.bounciness = 1;
-	wallBot = new Sprite(0, GameHeight, GameWidth*2, 8, 'k');
-	wallBot.color = 'black';
+    wallBot = new Sprite(0, GameHeight, GameWidth * 2, 8, 'k');
+    wallBot.color = 'black';
     wallBot.bounciness = 0;
     // contents of Jumpsurfaces group 
     Jumpsurfaces.add(wallBot);
@@ -67,124 +68,134 @@ function setup() {
     Sprites.add(wallRH);
     Sprites.add(wallL);
     Sprites.add(Player);
-   // Sprites.add(platforms);
+    // Sprites.add(platforms);
     //adding Gravity 
     world.gravity.y = 10;
     //Makes sprites disappear
     Sprites.visible = false;
-    restart = createButton("Restart");
-    restart.position(GameWidth / 2 - 50, GameHeight / 2 + 75);
-    restart.mousePressed(restartGame); // restarts game
-    restart.hide(); // hides button
+
 
 }
-function restartGame(){
-    background(Background);
-    Sprites.visible = true;
-    Coins();
-    platforms();
-    Gamestate = 'running'
-}
+//function restartGame(){
+//background(Background);
+//Sprites.visible = true;
+//Coins();
+//platforms();
+//Gamestate = 'running'
+//}
 // Function to make the platforms and make them spawn in random places 
-function platforms(){
+function platforms() {
     for (i = 1; i < Numberofplatforms; i++) {
 
         var platformX = GameWidth * Math.random();
-        var platformY = i * (GameHeight /(Numberofplatforms));
-		platform = new Sprite(platformX, platformY, 100, 8, 'k');
-		platform.bounciness = 0;
-		platform.friction = 5;
+        var platformY = i * (GameHeight / (Numberofplatforms));
+        platform = new Sprite(platformX, platformY, 100, 8, 'k');
+        platform.bounciness = 0;
+        platform.friction = 5;
         platform.color = 'White'
-		Sprites.add(platform);
+        Sprites.add(platform);
         Jumpsurfaces.add(platform);
         platform.image = (PlatformGrass);
         PlatformGrass.resize(100, 100);
-        
-	}
+
+    }
 
 }
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	background(Background);
-    if (kb.pressing('q')&& Gamestate != 'running') {
+    background(Background);
+    if (kb.pressing('q') && Gamestate != 'running') {
         Sprites.visible = true;
         Coins();
         platforms();
         Gamestate = 'running'
         startTime = millis()
     }
-    
-    if (Gamestate == 'running'){
+
+    if (Gamestate == 'running') {
         // This is the Score
         textSize(35);
-        text("Score:"+Score ,GameWidth/10-65, GameHeight/10-50);
+        text("Score:" + Score, GameWidth / 10 - 65, GameHeight / 10 - 50);
         textSize(35);
-		text("Timer:"+( GameTime - Math.floor((millis()-startTime)/1000 )), GameWidth/10-70, 70);
-		fill('Black');
+        text("Timer:" + (GameTime - Math.floor((millis() - startTime) / 1000)), GameWidth / 10 - 70, 70);
+        fill('Black');
     } else if (Gamestate == 'start') {
-        text("Press Q to start",GameWidth/2,GameHeight/2)
-        text("Instructions: Jump from platform to platform using WASD collect as many coins as possible before the time limit ends if you get them all good Job",GameWidth/2-GameWidth/2,GameHeight/2+GameHeight/10)
+        text("Press Q to start", GameWidth / 2, GameHeight / 2)
+        text("Instructions: Jump from platform to platform using WASD collect as many coins as possible before the time limit ends if you get them all good Job", GameWidth / 2 - GameWidth / 2, GameHeight / 2 + GameHeight / 10)
     }
-    if(Math.floor(millis()/1000) >= GameTime) {
-		// remove ervrything
-		coinGroup.remove();
-		Sprites.visible = false;
-		if(Score <= Coinnumber ){
-			background('pink');
-			text("End Game",GameWidth/2,GameHeight/2)
-            text("Your Score:"+Score,GameWidth/2-100,GameHeight/2+200)
-			fill('black');
+    if (Math.floor(millis() / 1000) >= GameTime) {
+        // remove ervrything
+        coinGroup.remove();
+        Sprites.visible = false;
+        if (Score <= Coinnumber) {
+            background('pink');
+            text("End Game", GameWidth / 2, GameHeight / 2)
+            text("Your Score:" + Score, GameWidth / 2 - 100, GameHeight / 2 + 200)
+            fill('black');
             restart.show()
         }
     }
     //KeyBoard controls   
     Player.colliding(Jumpsurfaces, Jump);
-    function Jump() {     
-        console.log("Jump");  
+
+    function Jump() {
+        console.log("Jump");
         if (kb.pressing('w')) {
             // Set sprite's velocity upwards
             Player.vel.y = -8;
-        }    
+        }
     }
     if (kb.pressing('a')) {
         // Set sprite's velocity to the left
         Player.vel.x = -5;
-    }
-    
-    else if (kb.pressing ('d')) {    
+    } else if (kb.pressing('d')) {
         // Set sprite's velocity to the right
         Player.vel.x = 5;
-    }
-  
-    else if (kb.pressing ('s')){
+    } else if (kb.pressing('s')) {
         // Set sprite's velocity downwards
         Player.vel.y = 5;
     }
+//Restart Game
+//function checkKey(_keyPressed) {
+    //if (_keyPressed === " " || _keyPressed === "Enter") {
+      //console.log("Game Started!");
+      //startGame();
+    //}
+    //else if (_keyPressed === 'r' || _keyPressed === 'R') {
+      //console.log("Game Restarted!");
+      //restartGame();
+    //}
+ //}
+  
+  //function keyPressed() {
+    //checkKey(key);
+  
+  //}
 }
 
 function Coins() {
     coinGroup = new Group();
 
-	for (i = 0; i < Coinnumber; i++) {
-		coin = new Sprite(GameWidth*Math.random(), GameHeight*Math.random(), 20, 'd');
-		coin.vel.x = 3;
-		coin.vel.y = 4;
-		coin.bounciness = 1;
-		coin.friction = 0;
+    for (i = 0; i < Coinnumber; i++) {
+        coin = new Sprite(GameWidth * Math.random(), GameHeight * Math.random(), 20, 'd');
+        coin.vel.x = 3;
+        coin.vel.y = 4;
+        coin.bounciness = 1;
+        coin.friction = 0;
         coin.color = 'gold'
-		coinGroup.add(coin);
+        coinGroup.add(coin);
         coin.image = (Coinimage);
         Coinimage.resize(50, 50);
-	}
+    }
 
-	// if any coin in the coin group colides with the Player call func2call
-	coinGroup.collides(Player, func2call);
+    // if any coin in the coin group colides with the Player call func2call
+    coinGroup.collides(Player, func2call);
 
-	function func2call(coin, circle) {
-		// Delete the coin that gets hit
-		coin.remove();
+    function func2call(coin, circle) {
+        // Delete the coin that gets hit
+        coin.remove();
         Score++;
     }
 }
